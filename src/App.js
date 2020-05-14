70e3a63fab4e45b938b81e6202c0f191b97b2ad2
Dominik Bil <dominobil112233@gmail.com>
2020-05-14 23:10:04 +0200

    Create app's state with exemplary inputs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
 import Header from './components/layout/Header'
+import TodoList from './components/TodoList'
+ 
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Do homework',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'Buy new keyboard',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Go for a walk with dog',
+        completed: false,
+      }
+    ]
+  }
+
+
+
   render() {
     return (
       <div className="app">
         <Header />
+        <TodoList />
       </div>
     )
   }

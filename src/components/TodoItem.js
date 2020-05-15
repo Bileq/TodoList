667b256d0dea874dd2ad455d54469d3015c457a2
Dominik Bil <dominobil112233@gmail.com>
2020-05-15 12:03:13 +0200

    Add line-through if todo is completed

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react'
 
 export class TodoItem extends Component {
+
+    itemStyle = () => {
+        return {
+            color: '#fff',
+            borderBottom: '#ff1605 solid 1px',
+            padding: '60px 0px 60px 60px',
+            fontSize: '26px',
+            textDecoration: this.props.todo.completed 
+            ? 'line-through' : 'none',
+        }
+    }
+
     render() {
         return (
             //Displaying each todo
-            <div style={itemStyle}>
+            <div style={this.itemStyle()}>
                 { this.props.todo.title }
             </div>
         )
     }
 }
 
-const itemStyle = {
-    color: '#fff',
-    borderBottom: '#ff1605 solid 1px',
-    padding: '60px 0px 60px 60px',
-    fontSize: '26px',
-}
 //To do later
 const xStyle = {
 

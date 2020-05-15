278159f8f29ea5e6ba8eec3f30ee098ce3ec4f06
Dominik Bil <dominobil112233@gmail.com>
2020-05-15 12:29:24 +0200

    Add x button

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,7 +6,7 @@ export class TodoItem extends Component {
         return {
             color: '#fff',
             borderBottom: '#ff1605 solid 1px',
-            padding: '60px 0px 60px 60px',
+            padding: '60px 50px 60px 50px',
             fontSize: '26px',
             textDecoration: this.props.todo.completed 
             ? 'line-through' : 'none',
@@ -18,6 +18,7 @@ export class TodoItem extends Component {
             //Displaying each todo
             <div style={this.itemStyle()}>
                 { this.props.todo.title }
+                <button style={xStyle}>x</button>
             </div>
         )
     }
@@ -25,7 +26,13 @@ export class TodoItem extends Component {
 
 //To do later
 const xStyle = {
-
+    float: 'right',
+    backgroundColor: '#ff1605',
+    borderRadius: '50%',
+    fontSize: '30px',
+    padding: '10px 20px',
+    border: 'none',
+    cursor: 'pointer',
 }
 export default TodoItem
 
